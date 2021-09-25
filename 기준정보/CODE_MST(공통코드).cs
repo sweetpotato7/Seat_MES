1ad7b519d23a30d06c33c6a5deffdc119b91d1da
sangmin <ksm9905@gmail.com>
2021-09-25 15:34:23 +0900

    공통코드 수정

--- 기준정보/CODE_MST(공통코드).cs
+++ 기준정보/CODE_MST(공통코드).cs
@@ -28,7 +28,7 @@ namespace MESProject.기준정보
                 sql.con.Close();
             }
             sql.con.Open();
-            //DGVLoad();
+            DGVLoad();
             disp(); // 전체조회
             CboSet(); // 콤보박스 세팅
         }
@@ -36,19 +36,18 @@ namespace MESProject.기준정보
         private void DGVLoad()
         {
             string[] DataPropertyName = new string[] { "PLANTCODE", "MAJORCODE", "MINORCODE", "CODENAME", "RELCODE1", "RELCODE2", "RELCODE3", "RELCODE4", "RELCODE5", "DISPLAYNO", "USEFLAG", "CREATE_USERID", "CREATE_DT",  "MODIFY_USERID", "MODIFY_DT" };
-            string[] HeaderText       = new string[] { "공장", "주코드", "부코드", "코드명", "코드명", "참조2", "참조3", "참조4", "참조5", "순서", "사용", "등록자", "등록일시", "수정자", "수정일시" };
+            string[] HeaderText       = new string[] { "공장", "주코드", "부코드", "코드명", "참조1", "참조2", "참조3", "참조4", "코드명", "순서", "사용", "등록자", "등록일시", "수정자", "수정일시" };
             string[] HiddenColumn     = null;
             float[] FillWeight = new float[] { 40, 100, 40, 100, 100, 100, 100, 100, 100, 40, 40, 100, 130, 100, 130 };
             Font StyleFont     = new Font("굴림", 9, FontStyle.Bold);
             Font BodyStyleFont = new Font("굴림", 9, FontStyle.Regular);
 
             //스타일 지정 밎 그리드에 데이터 바인드
-            HiddenColumn = new string[] { "PLANTCODE", "MINORCODE", "CODENAME", "RELCODE2", "RELCODE3", "RELCODE4", "RELCODE5", "DISPLAYNO", "USEFLAG", "CREATE_USERID", "CREATE_DT", "MODIFY_USERID", "MODIFY_DT" };
+            HiddenColumn = new string[] { "PLANTCODE", "MINORCODE", "CODENAME", "RELCODE1", "RELCODE2", "RELCODE3", "RELCODE4", "DISPLAYNO", "USEFLAG", "CREATE_USERID", "CREATE_DT", "MODIFY_USERID", "MODIFY_DT" };
             Main.DGVSetting(this.dataGridView1, DataPropertyName, 30, HeaderText, HiddenColumn, FillWeight, StyleFont, BodyStyleFont, 15);
-            HiddenColumn = new string[] { "MAJORCODE", "RELCODE1"};
+            HiddenColumn = new string[] { "MAJORCODE", "RELCODE5"};
             Main.DGVSetting(this.dataGridView2, DataPropertyName, 30, HeaderText, HiddenColumn, FillWeight, StyleFont, BodyStyleFont, 15);
 
-            //dataGridView2.ReadOnly = false;
             dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
         }
 
@@ -59,6 +58,7 @@ namespace MESProject.기준정보
             cboSearch.Items.Add("부코드");
             cboSearch.Items.Add("코드명");
             cboSearch.SelectedIndex = 0;
+            cboSearch.BackColor = SystemColors.Window;
         }
 
         public void DO_Search()
@@ -70,16 +70,18 @@ namespace MESProject.기준정보
                 switch(cboSearch.Text) // 쿼리문 생성
                 {
                     case "주코드":
-                        strqry = "SELECT DISTINCT MAJORCODE , RELCODE1 FROM TB_CODE_MST "
+                        strqry = "SELECT DISTINCT MAJORCODE , RELCODE5 FROM TB_CODE_MST "
                                + "WHERE MAJORCODE LIKE '%" + txtSearch.Text + "%'";
                         break;
                     case "부코드":
-                        strqry = "SELECT PLANTCODE, MINORCODE, CODENAME, RELCODE2, RELCODE3, RELCODE4, RELCODE5, DISPLAYNO, USEFLAG, CREATE_USERID, CREATE_DT, MODIFY_USERID, MODIFY_DT FROM TB_CODE_MST "
-                               + "WHERE MINORCODE LIKE '%" + txtSearch.Text + "%'";
+                        strqry = "SELECT PLANTCODE, MINORCODE, CODENAME, RELCODE1, RELCODE2, RELCODE3, RELCODE4, DISPLAYNO, USEFLAG, CREATE_USERID, CREATE_DT, MODIFY_USERID, MODIFY_DT FROM TB_CODE_MST "
+                               + "WHERE MINORCODE LIKE '%" + txtSearch.Text + "%'"
+                               + "ORDER BY DISPLAYNO";
                         break;
                     case "코드명": // 코드명은 부코드 이름?
-                        strqry = "SELECT PLANTCODE, MAJORCODE, RELCODE1, MINORCODE, CODENAME, RELCODE2, RELCODE3, RELCODE4, RELCODE5, DISPLAYNO, USEFLAG, CREATE_USERID, CREATE_DT, MODIFY_USERID, MODIFY_DT FROM TB_CODE_MST "
-                               + "WHERE CODENAME LIKE '%"  + txtSearch.Text + "%'";
+                        strqry = "SELECT PLANTCODE, MAJORCODE, RELCODE1, MINORCODE, CODENAME, RELCODE1, RELCODE2, RELCODE3, RELCODE4, DISPLAYNO, USEFLAG, CREATE_USERID, CREATE_DT, MODIFY_USERID, MODIFY_DT FROM TB_CODE_MST "
+                               + "WHERE CODENAME LIKE '%"  + txtSearch.Text + "%'"
+                               + "ORDER BY DISPLAYNO";
                         break;
                 }
 
@@ -115,7 +117,7 @@ namespace MESProject.기준정보
         {
             SqlCommand cmd = sql.con.CreateCommand();
             cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "SELECT DISTINCT MAJORCODE, RELCODE1 from TB_CODE_MST";
+            cmd.CommandText = "SELECT DISTINCT MAJORCODE, RELCODE5 from TB_CODE_MST";
             cmd.ExecuteNonQuery();
             DataTable dt = new DataTable();
             SqlDataAdapter da = new SqlDataAdapter(cmd);
@@ -125,7 +127,7 @@ namespace MESProject.기준정보
             SqlCommand cmd2 = sql.con.CreateCommand();
             cmd2.CommandType = CommandType.Text;
             //cmd2.CommandText = "select MINORCODE, CODENAME from TB_CODE_MST";
-            cmd2.CommandText = "SELECT PLANTCODE, MINORCODE, CODENAME, RELCODE2, RELCODE3, RELCODE4, RELCODE5, DISPLAYNO, USEFLAG, CREATE_USERID, CREATE_DT, MODIFY_USERID, MODIFY_DT FROM TB_CODE_MST";
+            cmd2.CommandText = "SELECT PLANTCODE, MINORCODE, CODENAME, RELCODE1, RELCODE2, RELCODE3, RELCODE4, DISPLAYNO, USEFLAG, CREATE_USERID, CREATE_DT, MODIFY_USERID, MODIFY_DT FROM TB_CODE_MST";
             cmd2.ExecuteNonQuery();
             DataTable dt2 = new DataTable();
             SqlDataAdapter da2 = new SqlDataAdapter(cmd2);
@@ -136,21 +138,29 @@ namespace MESProject.기준정보
         // MAJORCODE 클릭 시 MINORCODE, CODENAME 조회
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            string majorcode;
-            majorcode = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            try
+            {
+                string majorcode;
+                majorcode = dataGridView1.Rows[e.RowIndex].Cells[13].Value.ToString();
 
-            SqlCommand cmd = sql.con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            //cmd.CommandText = "select MINORCODE, CODENAME from TB_CODE_MST " +
-            cmd.CommandText = "SELECT PLANTCODE, MINORCODE, CODENAME, RELCODE2, RELCODE3, RELCODE4, RELCODE5, DISPLAYNO, USEFLAG, CREATE_USERID, CREATE_DT, MODIFY_USERID, MODIFY_DT FROM TB_CODE_MST " +
-                              "where MAJORCODE =" + "'" + majorcode + "'" + "";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            dataGridView2.DataSource = dt;
+                SqlCommand cmd = sql.con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                //cmd.CommandText = "select MINORCODE, CODENAME from TB_CODE_MST " +
+                cmd.CommandText = "SELECT PLANTCODE, MINORCODE, CODENAME, RELCODE1, RELCODE2, RELCODE3, RELCODE4, DISPLAYNO, USEFLAG, CREATE_USERID, CREATE_DT, MODIFY_USERID, MODIFY_DT FROM TB_CODE_MST " +
+                                  "where MAJORCODE = '" + majorcode + "' "
+                                + "ORDER BY DISPLAYNO";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                dataGridView2.DataSource = dt;
             
-            cboPlantCode.SelectedItem = "D001";
+                cboPlantCode.SelectedItem = "D001";
+            }
+            catch
+            {
+
+            }
         }
     }
 }
