666d67edf992deabc035dfb9539165a1690d3d53
sangmin <ksm9905@gmail.com>
2021-10-02 17:47:52 +0900

    작업지시 추가

---
+++ 공정관리/PROC_SEQ.Designer.cs
@@ -0,0 +1,205 @@
+﻿
+namespace MESProject.공정관리
+{
+    partial class PROC_SEQ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dtDate = new System.Windows.Forms.DateTimePicker();
+            this.cboALC = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 47F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1521, 841);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Controls.Add(this.dtDate);
+            this.panel1.Controls.Add(this.cboALC);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtQty);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1515, 41);
+            this.panel1.TabIndex = 3;
+            // 
+            // dtDate
+            // 
+            this.dtDate.Font = new System.Drawing.Font("굴림", 15F);
+            this.dtDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtDate.Location = new System.Drawing.Point(65, 7);
+            this.dtDate.Name = "dtDate";
+            this.dtDate.Size = new System.Drawing.Size(150, 30);
+            this.dtDate.TabIndex = 6;
+            // 
+            // cboALC
+            // 
+            this.cboALC.Font = new System.Drawing.Font("굴림", 15F);
+            this.cboALC.FormattingEnabled = true;
+            this.cboALC.Location = new System.Drawing.Point(279, 8);
+            this.cboALC.Name = "cboALC";
+            this.cboALC.Size = new System.Drawing.Size(135, 28);
+            this.cboALC.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 15F);
+            this.label3.Location = new System.Drawing.Point(420, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "수량";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 15F);
+            this.label2.Location = new System.Drawing.Point(228, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ALC";
+            // 
+            // txtQty
+            // 
+            this.txtQty.Font = new System.Drawing.Font("굴림", 15F);
+            this.txtQty.Location = new System.Drawing.Point(475, 7);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(83, 30);
+            this.txtQty.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 15F);
+            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "날짜";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.dataGridView2, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 50);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(1515, 788);
+            this.tableLayoutPanel2.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(1509, 309);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 318);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowTemplate.Height = 23;
+            this.dataGridView2.Size = new System.Drawing.Size(1509, 467);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // PROC_SEQ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1521, 841);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PROC_SEQ";
+            this.Text = "PROC_SEQ";
+            this.Load += new System.EventHandler(this.PROC_SEQ_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtDate;
+        private System.Windows.Forms.ComboBox cboALC;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
---
+++ 공정관리/PROC_SEQ.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MESProject.공정관리
+{
+    /// <summary>
+    /// DB에 추가해야될 것
+    /// INSERT INTO TB_CODE_MST ( PLANTCODE, MAJORCODE, MINORCODE, CODENAME, RELCODE5, DISPLAYNO, USEFLAG) VALUES ( 'D100', 'PLANFLAG', 'C', '생산완료', '계획상태', '3', 'Y')
+    /// INSERT INTO TB_CODE_MST ( PLANTCODE, MAJORCODE, MINORCODE, CODENAME, RELCODE5, DISPLAYNO, USEFLAG) VALUES ( 'D100', 'PLANFLAG', 'I', '투입중', '계획상태', '2', 'Y')
+    /// INSERT INTO TB_CODE_MST ( PLANTCODE, MAJORCODE, MINORCODE, CODENAME, RELCODE5, DISPLAYNO, USEFLAG) VALUES ( 'D100', 'PLANFLAG', 'R', '계획수립', '계획상태', '1', 'Y')
+    /// PLANDATE 형식 6자리 변경 (211002)
+    /// ALTER TABLE TB_PLAN_MST ALTER COLUMN PLANDATE VARCHAR(6) NOT NULL;
+    /// 
+    /// </summary>
+
+
+
+
+    public partial class PROC_SEQ : Form
+    {
+        Function func = new Function();
+        SQL sql = new SQL();
+        SqlDataAdapter da;
+        DataTable dt;
+        string strqry = string.Empty;
+
+        public PROC_SEQ()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        private void PROC_SEQ_Load(object sender, EventArgs e)
+        {
+            DGVLoad();
+            func.CboLoad(cboALC, "TB_ITEM_MST", "ITEMCODE", false, "ITEMTYPE", "ALC");
+            Do_Search();
+        }
+
+        private void DGVLoad()
+        {
+            // DGV1
+            string[] DataPropertyName = new string[] { "PLANTCODE", "PLANDATE", "PLANSEQ", "ALC_CD", "PLANQTY", "ORDERNO", "PRODQTY", "PLANFLAG", "CREATE_USERID", "CREATE_DT", "MODIFY_USERID", "MODIFY_DT" };
+            string[] HeaderText       = new string[] { "공장", "날짜", "순서", "ALC", "수량", "작업지시번호", "생산수량", "계획상태", "등록자", "등록일자", "수정자", "수정일자" };
+            string[] HiddenColumn     = new string[] { "공장" };
+            float[] FillWeight        = new float[] { 40, 100, 40, 100, 40, 130, 40, 40, 100, 130, 100, 130 };
+            Font StyleFont     = new Font("굴림", 9, FontStyle.Bold);
+            Font BodyStyleFont = new Font("굴림", 9, FontStyle.Regular);
+
+            //스타일 지정 밎 그리드에 데이터 바인드
+            Main.DGVSetting(this.dataGridView1, DataPropertyName, 30, HeaderText, HiddenColumn, FillWeight, StyleFont, BodyStyleFont, 12);
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            // DGV2 수정!
+            DataPropertyName = new string[] { "PLANTCODE", "PLANDATE", "PLANSEQ", "ALC_CD", "PLANQTY", "ORDERNO", "PRODQTY", "PLANFLAG", "CREATE_USERID", "CREATE_DT", "MODIFY_USERID", "MODIFY_DT" };
+            HeaderText       = new string[] { "공장", "날짜", "순서", "ALC", "수량", "작업지시번호", "생산수량", "계획상태", "등록자", "등록일자", "수정자", "수정일자" };
+            HiddenColumn     = new string[] { "공장" };
+            FillWeight       = new float[]  { 40, 100, 40, 100, 40, 130, 40, 40, 100, 130, 100, 130 };
+            StyleFont     = new Font("굴림", 9, FontStyle.Bold);
+            BodyStyleFont = new Font("굴림", 9, FontStyle.Regular);
+
+            //스타일 지정 밎 그리드에 데이터 바인드
+            Main.DGVSetting(this.dataGridView2, DataPropertyName, 30, HeaderText, HiddenColumn, FillWeight, StyleFont, BodyStyleFont, 12);
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+
+
+        #region ========== CRUD
+        public void Do_Search() // 검색
+        {
+            strqry = "SELECT * FROM TB_PLAN_MST";
+            if (dtDate.Value.ToString("yyMMdd") != DateTime.Now.ToString("yyMMdd"))
+            {
+                strqry += " WHERE PLANDATE = '" + dtDate.Value.ToString("yyMMdd") + "'";
+            }
+            strqry += " ORDER BY PLANDATE, PLANSEQ";
+
+            try
+            {
+                sql.con.Open();
+                dt = func.GetDataTable2(strqry);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sql.con.Close();
+            }
+        }
+        public void Do_Insert()
+        {
+            string sDate = dtDate.Value.ToString("yyMMdd");
+            string sALC  = cboALC.Text;
+            int iQty     = int.Parse(txtQty.Text);
+
+            SqlCommand cmd = new SqlCommand("저장프로시저이름", sql.con);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@PLANDATE", SqlDbType.VarChar, 6).Value  = dtDate.Value.ToString("yyMMdd");
+            cmd.Parameters.Add("@ALC_CD",   SqlDbType.VarChar, 20).Value = cboALC.Text;
+            cmd.Parameters.Add("@PLANQTY",  SqlDbType.Int).Value         = txtQty.Text;
+
+            //SqlCommand cmd = new SqlCommand("EXEC 프로시저명 값1, 값2", conn);
+
+            cmd.ExecuteNonQuery();
+
+        }
+        #endregion
+
+    }
+}
