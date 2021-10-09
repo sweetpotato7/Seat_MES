fb28e23adebdac01f82d378db6bf979816360c51
효주 이 <gywn7416@naver.com>
2021-10-09 17:48:01 +0900

    ㅇㅇ

--- 공정관리/PROC_MST(공정관리).cs
+++ 공정관리/PROC_MST(공정관리).cs
@@ -22,41 +22,19 @@ namespace MESProject.공정관리
             InitializeComponent();
         }
 
-        public void Spec_dv()
-        {
-            DGVLoad_Spec();
-            strqry = "select * from TB_SPEC";
-            dataGridView2.DataSource = func.GetDataTable(strqry);
-        }
-
         public void ProcSeq_dv()
         {
             DGVLoad_ProcSeq();
-            strqry = "select * from TB_PROC_SEQ";
+            string proc_cd = "010";
+            strqry = "select PROC_SEQ, STEP_CD, STEP_NAME from TB_PROC_SEQ WHERE PROC_CD =" + "'" + proc_cd + "'" + "";
             dataGridView3.DataSource = func.GetDataTable(strqry);
-        }
-
-        private void DGVLoad_Spec()
-        {
-            string[] DataPropertyName = new string[] { "PLANTCODE", "ITEMCODE", "CARCODE", "SEATTYPE", "SPEC1", "SPEC2", "SPEC3", "SPEC4", "SPEC5", "SPEC6", "LOTNO", "USEFLAG", "CREATE_USERID", "CREATE_DT", "MODIFY_USERID", "MODIFY_DT" };
-            string[] HeaderText = new string[] { "공장코드", "품번", "차종", "시트타입", "지역", "트랙", "시트", "헤드레스트", "색상", "SAB", "사용여부", "고유번호", "등록자", "등록일시", "수정자", "수정일시" };
-            float[] FillWeight = new float[] { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
-            Font StyleFont = new Font("맑은고딕", 11, FontStyle.Bold);
-            Font BodyStyleFont = new Font("맑은고딕", 11, FontStyle.Regular);
-
-
-            //스타일 지정 밎 그리드에 데이터 바인드
-            Main.DGVSetting(this.dataGridView2, DataPropertyName, 30, HeaderText, null, FillWeight, StyleFont, BodyStyleFont, 16);
-            dataGridView2.ReadOnly = true;
-            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            dataGridView2.RowHeadersVisible = false;
         }
 
         private void DGVLoad_ProcSeq()
         {
-            string[] DataPropertyName = new string[] { "PLANTCODE", "LINE_CD", "PROC_CD", "PROC_NAME", "STEP_CD", "STEP_NAME", "PROC_SEQ", "INCOMDE", "WORK_START", "LOTNO", "WORK_END", "CREATE_USERID", "CREATE_DT", "MODIFY_USERID", "MODIFY_DT" };
-            string[] HeaderText = new string[] { "공장", "라인번호", "공정코드", "공정", "작업코드", "작업", "작업순서", "INCOMDE", "WORK_START", "LOTNO", "WORK_END", "등록자", "등록일시", "수정자", "수정일시" };
-            float[] FillWeight = new float[] { 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100 };
+            string[] DataPropertyName = new string[] { "PROC_SEQ", "STEP_CD", "STEP_NAME", "RESULT"};
+            string[] HeaderText = new string[] { "순서", "작업코드", "작업", "결과"};
+            float[] FillWeight = new float[] { 100, 100, 100, 100};
             Font StyleFont = new Font("맑은고딕", 11, FontStyle.Bold);
             Font BodyStyleFont = new Font("맑은고딕", 11, FontStyle.Regular);
 
@@ -75,8 +53,16 @@ namespace MESProject.공정관리
                 sql.con.Close();
             }
             sql.con.Open();
-            Spec_dv();
             ProcSeq_dv();
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int i;
+            i = dataGridView3.SelectedCells[0].RowIndex;
+            dataGridView3.Rows[i].Cells[3].Value.ToString()
+            // 셀 더블 클릭 시 결과 셀에 "완료" 입력
+
         }
     }
 }
